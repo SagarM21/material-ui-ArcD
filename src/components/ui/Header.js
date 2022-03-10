52b6c00676f05be024c08f06e10d71957a45f35a
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-03-10 18:42:44 +0530

    Hero Block & Services Block - Landing Page

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -74,6 +74,9 @@ const useStyles = makeStyles((theme) => ({
 		marginLeft: "50px",
 		marginRight: "25px",
 		height: "45px",
+		"&:hover": {
+			backgroundColor: theme.palette.secondary.light,
+		},
 	},
 	menu: {
 		backgroundColor: theme.palette.common.blue,
@@ -160,7 +163,7 @@ const Header = (props) => {
 			selectedIndex: 1,
 		},
 		{
-			name: "Mobile App Development",
+			name: "iOS/Android App Development",
 			link: "/mobileapps",
 			activeIndex: 1,
 			selectedIndex: 2,
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,6 +2,7 @@ import { createTheme } from "@material-ui/core/styles";
 
 const arcBlue = "#0B72B9";
 const arcOrange = "#FFBA60";
+const arcGrey = "#868686";
 const theme = createTheme({
 	palette: {
 		common: {
@@ -27,6 +28,33 @@ const theme = createTheme({
 			fontSize: "1rem",
 			textTransform: "none",
 			color: "white",
+		},
+		h2: {
+			fontFamily: "Raleway",
+			fontWeight: 700,
+			fontSize: "2.5rem",
+			color: `${arcBlue}`,
+			lineHeight: 1.5,
+		},
+		h4: {
+			fontFamily: "Raleway",
+			fontWeight: 700,
+			fontSize: "1.75rem",
+			color: `${arcBlue}`,
+		},
+		subtitle1: {
+			fontWeight: 300,
+			fontSize: "1.25rem",
+			color: `${arcGrey}`,
+		},
+		learnButton: {
+			borderColor: `${arcBlue}`,
+			color: `${arcBlue}`,
+			borderWidth: 2,
+			textTransform: "none",
+			borderRadius: 50,
+			fontFamily: "Roboto",
+			fontWeight: "bold",
 		},
 	},
 });
